87eaa6face9282006acf9bcd3e58dcf7bde924a5
Sarah <sarahpardeike@gmail.com>
2023-04-19 15:52:12 +0200

    changed some stuff

--- creator.py
+++ creator.py
@@ -13,6 +13,38 @@ character5 = input("Character 5: ")
 print("***YOUR CHARACTERS ARE COMPLETE***")
 print()
 
+
+role_number = random.randint(1,3)
+
+if role_number == 1:
+    char_role = "Warrior"
+elif role_number == 2:
+    char_role = "Wizard"
+else:
+    char_role = "Potato"
+    
+strength = random.randint(5,10)
+magic = random.randint(5,10)
+health = random.randint(5,10)
+
+if char_role == "Warrior":
+    strength *= 3
+
+elif char_role == "Wizard":
+    magic *= 3
+
+elif char_role == "Potato":
+    health *= 3
+
+print(character1, "is a", char_role)
+print("Strength:", strength)
+print("Magic:", magic)
+print("Health:", health)
+
+print()
+print("------")
+print()
+
 role_number = random.randint(1,3)
 
 if role_number == 1:
@@ -22,7 +54,6 @@ elif role_number == 2:
 else:
     char_role = "Potato"
 
-print(character1, "is a", char_role)
 strength = random.randint(5,10)
 magic = random.randint(5,10)
 health = random.randint(5,10)
@@ -35,16 +66,25 @@ elif char_role == "Wizard":
 
 elif char_role == "Potato":
     health *= 3
-
-print("Strength:", strength)
-print("Magic:", magic)
-print("Health:", health)
-
-print()
-print("------")
-print()
 
 print(character2, "is a", char_role)
+print("Strength:", strength)
+print("Magic:", magic)
+print("Health:", health)
+
+print()
+print("------")
+print()
+
+role_number = random.randint(1,3)
+
+if role_number == 1:
+    char_role = "Warrior"
+elif role_number == 2:
+    char_role = "Wizard"
+else:
+    char_role = "Potato"
+
 strength = random.randint(5,10)
 magic = random.randint(5,10)
 health = random.randint(5,10)
@@ -57,16 +97,25 @@ elif char_role == "Wizard":
 
 elif char_role == "Potato":
     health *= 3
-
-print("Strength:", strength)
-print("Magic:", magic)
-print("Health:", health)
-
-print()
-print("------")
-print()
 
 print(character3, "is a", char_role)
+print("Strength:", strength)
+print("Magic:", magic)
+print("Health:", health)
+
+print()
+print("------")
+print()
+
+role_number = random.randint(1,3)
+
+if role_number == 1:
+    char_role = "Warrior"
+elif role_number == 2:
+    char_role = "Wizard"
+else:
+    char_role = "Potato"
+
 strength = random.randint(5,10)
 magic = random.randint(5,10)
 health = random.randint(5,10)
@@ -79,29 +128,8 @@ elif char_role == "Wizard":
 
 elif char_role == "Potato":
     health *= 3
-
-print("Strength:", strength)
-print("Magic:", magic)
-print("Health:", health)
-
-print()
-print("------")
-print()
 
 print(character4, "is a", char_role)
-strength = random.randint(5,10)
-magic = random.randint(5,10)
-health = random.randint(5,10)
-
-if char_role == "Warrior":
-    strength *= 3
-
-elif char_role == "Wizard":
-    magic *= 3
-
-elif char_role == "Potato":
-    health *= 3
-
 print("Strength:", strength)
 print("Magic:", magic)
 print("Health:", health)
@@ -110,7 +138,15 @@ print()
 print("------")
 print()
 
-print(character5, "is a", char_role)
+role_number = random.randint(1,3)
+
+if role_number == 1:
+    char_role = "Warrior"
+elif role_number == 2:
+    char_role = "Wizard"
+else:
+    char_role = "Potato"
+
 strength = random.randint(5,10)
 magic = random.randint(5,10)
 health = random.randint(5,10)
@@ -124,6 +160,7 @@ elif char_role == "Wizard":
 elif char_role == "Potato":
     health *= 3
 
+print(character5, "is a", char_role)
 print("Strength:", strength)
 print("Magic:", magic)
 print("Health:", health)
